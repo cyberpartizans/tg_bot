1372511e099a6869964e18e17be2eaf81b9510ea
ivanovivan12589 <kuklovodn97@protonmail.com>
2020-10-07 19:20:46 +0300

    Fix client disconeccted

--- parse_channels_bot.py
+++ parse_channels_bot.py
@@ -26,18 +26,23 @@ with open("config.txt") as reader:
     api_hash = lines[2][:-1]
     phone_number = lines[3][:-1]
 
+client = TelegramClient(client_name, api_id, api_hash)
+client.start()
+print("Client Created")
+# Ensure you're authorized
+if not client.is_user_authorized():
+    client.send_code_request(phone_number)
+    try:
+        client.sign_in(phone_number, input('Enter the code: '))
+    except SessionPasswordNeededError:
+        client.sign_in(password=input('Password: '))
+
 async def find_messages():
-    client = TelegramClient(client_name, api_id, api_hash)
-    client.start()
-    print("Client Created")
-    # Ensure you're authorized
-    if not client.is_user_authorized():
-        await client.send_code_request(phone_number)
-        try:
-            await client.sign_in(phone_number, input('Enter the code: '))
-        except SessionPasswordNeededError:
-            await client.sign_in(password=input('Password: '))
-            
+    global offset_id
+    global total_messages
+    global limit
+    global all_messages
+    global total_count_limit
     black_book = await client.get_entity("t.me/BlackBookBelarus")
     while True:
         print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
@@ -56,12 +61,10 @@ async def find_messages():
         messages = history.messages
         print(f"Add new messages with len = {len(messages)}")
         for message in messages:
+            print(message)
             all_messages.append(message.to_dict())
         offset_id = messages[len(messages) - 1].id
         total_messages = len(all_messages)
-        if total_count_limit != 0 and total_messages >= total_count_limit:
-            break
 
-asyncio.run(find_messages())
-
-# client.run_until_disconnected()
+with client:
+    client.loop.run_until_complete(find_messages())
